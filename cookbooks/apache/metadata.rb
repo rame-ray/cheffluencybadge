7620a1089ef3d4496568bcdee16567fc60d723e8
Ramesh <rameshrm15@gmail.com>
2017-05-10 15:28:56 +0000

    added motd and changed default for node attribute

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -4,8 +4,10 @@ maintainer_email 'rameshrm15@gmail.com.com'
 license 'All Rights Reserved'
 description 'Installs/Configures apache'
 long_description 'Installs/Configures apache'
-version '0.1.0'
+version '0.2.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'mysql', '~> 7.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
