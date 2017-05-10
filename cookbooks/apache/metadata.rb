02071dd69b0ece09d27bf7a99eb79ea80a549f16
Ramesh <rameshrm15@gmail.com>
2017-05-10 15:32:08 +0000

    reverting a change

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -7,8 +7,6 @@ long_description 'Installs/Configures apache'
 version '0.2.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-depends 'mysql', '~> 7.0'
-
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
